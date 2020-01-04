7298ca546f6cd0c31fb57c450b3a776767806183
Guilherme Schardong <schardong.gg@gmail.com>
2020-01-04 18:16:55 -0300

    Removed the release main function and adjusted the camera FOVY.

--- src/main.cpp
+++ src/main.cpp
@@ -86,7 +86,6 @@ void trace_full(HitObject* world, Camera& cam, ImgData& img_data,
   }
 }
 
-#ifndef _DEBUG
 int main(int argc, char** argv)
 {
   const int N_CHANNELS = 3;
@@ -122,7 +121,7 @@ int main(int argc, char** argv)
   vec3 up = vec3(0.f, 1.f, 0.f);
   float aperture = 0.0f;
   float dist_to_focus = 10.f;
-  Camera cam(pos, lookat, up, 60, float(nx) / float(ny), aperture,
+  Camera cam(pos, lookat, up, 20, float(nx) / float(ny), aperture,
              dist_to_focus);
   vector<int> data;
 
@@ -138,43 +137,3 @@ int main(int argc, char** argv)
 
   return 0;
 }
-#else
-int main(int argc, char** argv)
-{
-  const int N_CHANNELS = 3;
-  int nx = 200;
-  int ny = 100;
-  int n_samples = 100;
-
-  if (argc > 1)
-    nx = atoi(argv[1]);
-  if (argc > 2)
-    ny = atoi(argv[2]);
-  if (argc > 3)
-    n_samples = atoi(argv[3]);
-
-  HitObject* world = two_spheres();//book1_cover(true);
-
-  vec3 pos = vec3(13.f, 2.f, 3.f);
-  vec3 lookat = vec3(0.f, 0.f, 0.f);
-  vec3 up = vec3(0.f, 1.f, 0.f);
-  float aperture = 0.1f;
-  float dist_to_focus = 10.f;
-  Camera cam(pos, lookat, up, 20, float(nx) / float(ny), aperture,
-             dist_to_focus);
-  vector<int> data;
-
-  ImgData img_data = {
-                 .nx = nx,
-                 .ny = ny,
-                 .n_channels = N_CHANNELS,
-                 .data = data,
-  };
-
-  trace_full(world, cam, img_data, n_samples);
-  to_ppm(img_data);
-
-  //delete world;
-  return 0;
-}
-#endif
